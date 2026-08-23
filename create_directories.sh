ca54ce666dbd272b471433ac2ee1d7c62df67909
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2026-08-24 01:56:25 +0300

    Don't create redundant dirs

--- create_directories.sh
+++ create_directories.sh
@@ -3,6 +3,6 @@
 INSTANCE_NAME=${1:-vsftpd-1}
 BASE_DIR="${HOME}/${INSTANCE_NAME}"
 
-mkdir -p "${BASE_DIR}"/{ftp,mariadb/{libmysql,data},log/{vsftpd,mariadb}}
+mkdir -p "${BASE_DIR}"/{ftp,mariadb/data,log/{vsftpd,mariadb}}
 
 
