777df6bf2dcd553bafb09df6e7431fb52a26a781
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2026-08-26 21:36:30 +0300

    Add user add advice

--- create_directories.sh
+++ create_directories.sh
@@ -15,9 +15,10 @@ if [ "$(id -u)" = 0 ]; then
     chown -R "${MARIADB_OWNERSHIP}" "${BASE_DIR}/log/mariadb"
     echo "Directory structure created"
 else
-    echo "You need to execute 'chown -R "${VSFTPD_OWNERSHIP}" "${BASE_DIR}/ftp"' with root privileges, presumably"
+    echo "You need to execute 'chown -R "${VSFTPD_ROOT_OWNERSHIP}" "${BASE_DIR}/ftp"' with root privileges, presumably"
     echo "You need to execute 'chown -R "${VSFTPD_ROOT_OWNERSHIP}" "${BASE_DIR}/log/vsftpd"' with root privileges, presumably"
     echo "You need to execute 'chown -R "${MARIADB_OWNERSHIP}" "${BASE_DIR}/mariadb"' with root privileges, presumably"
     echo "You need to execute 'chown -R "${MARIADB_OWNERSHIP}" "${BASE_DIR}/log/mariadb"' with root privileges, presumably"
 fi
 
+echo "For each new FTP user, you must create their data folder structure under "${BASE_DIR}/ftp". For example, for user 'alice' you need to create "${BASE_DIR}/ftp/alice/data". The "${BASE_DIR}/ftp" and "${BASE_DIR}/ftp/alice" must both be owned by "${VSFTPD_ROOT_OWNERSHIP}". The "${BASE_DIR}/ftp/alice/data" folder must belong to "${VSFTPD_OWNERSHIP}". The "${BASE_DIR}/ftp/alice/data" and the "${BASE_DIR}/ftp" folders must have 0755 permissions. The "${BASE_DIR}/ftp/alice" folder must have 0555 permissions. Good luck with not messing this up."
